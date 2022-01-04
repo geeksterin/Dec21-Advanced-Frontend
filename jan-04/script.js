1c73db10c0770df40e1613768b3705e9277c4969
sudhanshu kumar <sudhanshukumar8816@gail.com>
2022-01-04 22:05:06 +0530

    Jan 04

---
+++ jan-04/script.js
@@ -0,0 +1,71 @@
+const heading = document.querySelector('.heading')
+const resetButton =document.querySelector('.reset-btn') 
+const gameCells = document.querySelectorAll('.cell')
+
+
+var isXturn=true;
+var count =0;
+
+const checkGameStatus=()=>{
+    const index0ClassList=gameCells[0].classList;
+    const index1ClassList=gameCells[1].classList;
+    const index2ClassList=gameCells[2].classList;
+    const index3ClassList=gameCells[3].classList;
+    const index4ClassList=gameCells[4].classList;
+    const index5ClassList=gameCells[5].classList;
+    const index6ClassList=gameCells[6].classList;
+    const index7ClassList=gameCells[7].classList;
+    const index8ClassList=gameCells[8].classList;
+   
+
+    if(index0ClassList.contains('O')){
+        if(index1ClassList.contains('O') && index2ClassList.contains('O')){
+            alert("O won")
+        }
+    }
+
+}
+
+
+
+function cellHandler(event){
+   const classList= event.target.classList;
+   if(classList.length>1){
+       return;
+   }
+   if(isXturn){
+        classList.add("X")
+        isXturn=false;
+        heading.textContent="O's turn"
+
+   }else{
+       classList.add('O')
+       isXturn=true;
+       heading.textContent="X's turn"
+   }
+   count=count+1
+   console.log(count)
+   if(count >=5){
+       checkGameStatus()
+   }
+}
+
+
+
+
+
+
+for(cell of gameCells){
+    cell.addEventListener('click',cellHandler)
+}
+
+const resetHandler=()=>{
+    window.location.reload()
+}
+
+
+resetButton.addEventListener('click',resetHandler)
+
+
+
+
