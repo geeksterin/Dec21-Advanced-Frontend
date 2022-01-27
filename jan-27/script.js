d9fffed2eb0ae89411f942fdbc8e350c32e5eaef
sudhanshu kumar <sudhanshukumar8816@gail.com>
2022-01-27 23:17:58 +0530

    jan-27

---
+++ jan-27/script.js
@@ -0,0 +1,99 @@
+'use strict'
+// var x=110
+// function substract(a,b){
+//     var c= a-b;
+//     console.log(c)
+
+//     function add(a,b){
+        
+//         var c1= a-b;
+//         console.log(c)
+//         console.log(x)
+//     }
+   
+//     add(10,15)
+// }
+
+// substract(30,29)
+
+// console.log(x)
+// var x=10;
+
+
+// console.log(y)
+// const y=10
+
+
+// console.log(z)
+// let z=10
+
+// console.log(add(2,4))
+// function add(a,b){
+//     return a+b
+// }
+
+
+// console.log(x)
+// var x=11
+
+
+// console.log(add(2,3))
+// function add(a,b){
+//     return a+b
+// }
+
+// var t=10
+
+// console.log(add(2,3))
+// function add (a,b){
+//     return a+b
+// }
+
+// console.log(this)
+// var x=10
+// let y=10
+// function test(){
+//     console.log(this)
+// }
+// test()
+// var x=32
+// const obj = {
+//     x:42,
+//     printX:()=>{
+//         console.log(this.x)
+//     }
+// }
+
+// obj.printX()
+
+
+// const add =()=>{
+//     console.log(this)
+// }
+
+// add()
+
+
+// function test1(){
+//     console.log(this)
+//     const test2 =()=>{
+//         console.log(this)
+//     }
+//     test2()
+// }
+
+// test1()
+
+
+// function testingClosure (){
+//     let x=10;
+//     return function(){
+//         x++;
+//         console.log(x)
+//     }
+// }
+
+// const tester = testingClosure()
+
+// tester()
+// tester()
